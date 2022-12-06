aa92c458ba0c109d87c812bd1c4cdbbde731ce3f
Naufal Tsaqif A.N <ncgamer21@gmail.com>
2022-12-06 09:49:48 +0700

    tambahan

--- proses.php
+++ proses.php
@@ -1,59 +1,57 @@
 <?php
+//Define name spaces
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-function send($target, $pesan){
-    $curl = curl_init();
-    
-    $data = [
-        'target' => $target,
-        'message' => $pesan
-    ];
-    
-    curl_setopt($curl, CURLOPT_HTTPHEADER,
-        array(
-            "Authorization: TOKEN MU",
-        )
-    );
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-    curl_setopt($curl, CURLOPT_URL, "https://api.fonnte.com/send");
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    $result = curl_exec($curl);
-    curl_close($curl);
-    
-    return $result;
-}
 
-if (isset($_POST['nomer']) and isset($_POST['nama']) and isset($_POST['pesan'])) {
-    
-    $nomer = $_POST['nomer'];
-    $nama = $_POST['nama'];
-    $pesan = $_POST['pesan'];
-    
-    if ($nomer == "" or $nama == "" or $pesan == "") {
-        echo "Ulangi Lagi"; exit;
-    } else {
+    if(isset($_POST['submit'])){
         
-        if(strpos(substr($nomer,0,3), '08') !== false){
-        	$awal = str_replace("08", "628", substr($nomer,0,3));
-        	$nomer = $awal. substr($nomer,3);
-        }
-        
-        $kirim = json_decode(send($nomer, "Halo ada yang mengisi form dengan data\n\nnama: ".$nama."\npesan: ".$pesan."\n\nsegera follow up!\n\n*ini pesan untuk admin"), true);
-        $kirim1 = json_decode(send($nomer, "Terimakasih telah menghubungi kami. ini adalah pesan notifikasi bahwa kami telah menerima pesan anda. kami akan segera menghubungi kembali.\n\nTerimakasih\n\n*ini pesan untuk customer"), true);
-        if ($kirim['status'] == true and $kirim1['status'] == true) {
-            echo "Pesan telah dikirim"; exit;
-        } else {
-            echo "Pesan gagal dikirim"; exit;
-        }
-            
-    }
-    
-} else {
-    echo "Silahkan coba lagi";
+        $tanggal = $_['time'];
+        $nama = $_POST['nama'];
+        $telpon = $_POST['telpon'];
+        $nomor = $_POST['nomeja'];
+        $jml = $_POST['jumlah'];        
 }
 
 
+//Include required PHPMailer files
+	require 'includes/PHPMailer.php';
+	require 'includes/SMTP.php';
+	require 'includes/Exception.php';
 
+//Create instance of PHPMailer
+	$mail = new PHPMailer();
+//Set mailer to use smtp
+	$mail->isSMTP();
+//Define smtp host
+	$mail->Host = "smtp.gmail.com";
+//Enable smtp authentication
+	$mail->SMTPAuth = true;
+//Set smtp encryption type (ssl/tls)
+	$mail->SMTPSecure = "tls";
+//Port to connect smtp
+	$mail->Port = "587";
+//Set gmail username
+	$mail->Username = "ncgamer21@gmail.com";
+//Set gmail password
+	$mail->Password = "satpukfyvydjspnu";
+//Email subject
+	$mail->Subject = "Order";
+//Set sender email
+	$mail->setFrom('sritoyo21@gmail.com');
+//Enable HTML
+	$mail->isHTML(true);
+//Email body
+	$mail->Body = "<h1>ORDER</h1></br><p>Nama = $nama</p></br><p>Nomor Telepon = $telpon</p></br><p>Tanggal Pesanan = $tanggal</p></br><p>Nomor Meja = $nomor</p></br><p>Jumlah Orang = $jml</p>";
+//Add recipient
+	$mail->addAddress('ncgamer21@gmail.com');
+//Finally send email
+	if ( $mail->send() ) {
+		echo "Email Sent..!";
+	}else{
+		echo "Message could not be sent. Mailer Error: "{$mail->ErrorInfo};
+	}
+//Closing smtp connection
+	$mail->smtpClose();
 ?>
