65f7037769ec6da81d0f1b91aaa7ae583cbd3073
Naufal Tsaqif A.N <69290981+NTsaqif@users.noreply.github.com>
2023-01-01 13:13:32 +0700

    update

--- alternatif/contact.php
+++ alternatif/contact.php
@@ -125,7 +125,7 @@
           <div class="col-lg-3 col-md-6 mb-5 mb-md-5">
             <div class="ftco-footer-widget mb-4">
               <h2 class="ftco-heading-2">About Us</h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+              <p>Tikum Cookery and Coffee berdiri pada bulan februari tahun 2022 oleh sekumpulan penikmat kopi dari kota jember.</p>
               <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
                 <li class="ftco-animate"><a href="https://gofood.link/a/yQykwQC"><span class="icon-shopping_cart"></span></a></li>
                 <li class="ftco-animate"><a href="https://instagram.com/tikum.cookeryandcoffee?igshid=YmMyMTA2M2Y="><span class="icon-instagram"></span></a></li>
